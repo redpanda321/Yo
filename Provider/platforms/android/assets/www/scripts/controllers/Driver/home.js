f32ec80304664ac6c2132a6a6a776cd6a0d14858
redpanda321 <redpanda321@hotmail.com>
2016-03-01 09:12:17 -0700

    v0.0.3

--- Provider/platforms/android/assets/www/scripts/controllers/Driver/home.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('taxigoDriverApp')
-    .controller('homeCtrl', ['$scope', 'gmaps', '$timeout', 'auth', '$state', function ($scope, gmaps, $timeout, auth, $state) {
-
-    }]);
-
